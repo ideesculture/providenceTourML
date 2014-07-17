8459efe1655fc66251afe86f74773bc35293dd16
GautierMichelin <gm@ideesculture.com>
2014-07-17 15:34:28 +0200

    Removing hard-coded URL... no way, hard coded ?

--- views/preview_html.php
+++ views/preview_html.php
@@ -1,13 +1,14 @@
 <?php
 	MetaTagManager::addLink('stylesheet', __CA_URL_ROOT__."/app/plugins/tourML/css/iphone-iframe.css",'text/css');	
-        $tour_id = $this->getVar('id');
+    $tour_id = $this->getVar('id');
+    $target = "http://".__CA_SITE_HOSTNAME__ . "/".__CA_URL_ROOT__."app/plugins/providenceTourML/lib/tap-webapp/index.php?id=".$tour_id;
 ?>
 <script>
     localStorage.clear();
 </script>
 <H1>Preview</H1>
 <div id="tap-webapp">
-    <iframe src="http://collective.local/app/plugins/providenceTourML/lib/tap-webapp/index.php?id=<?php print($tour_id); ?>" height="568" width="320" frameBorder="0"></iframe>
+    <iframe src="<?php print $target; ?>" height="568" width="320" frameBorder="0"></iframe>
 </div> 
 <div class="editorBottomPadding"><!-- empty --></div>
 <div style="clear:both;"><!-- EMPTY --></div>
