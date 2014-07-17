dbe1cdcc4e1a6334d840dbc1d25f840d0fc611e6
GautierMichelin <gm@ideesculture.com>
2014-07-17 15:53:18 +0200

    Changing path

--- views/index_html.php
+++ views/index_html.php
@@ -13,7 +13,7 @@ foreach($va_available_tours as $tour) : ?>
 		<td><?php print $tour["tour_id"]; ?></td>
 		<td><?php print $tour["tour_code"]; ?></td>
 		<td><?php print $tour["preferred_labels"]; ?></td>
-		<td><a href="http://<?php print __CA_SITE_HOSTNAME__ . "/".__CA_URL_ROOT__."index.php/providenceTourML/providenceTourML/preview/id/".$tour["tour_id"]; ?>" class='form-button'><span class='form-button'><img src='/gestion/themes/default/graphics/buttons/lens.png' border='0' alt='Preview' class='form-button-left' style='padding-right: 10px;'/>Preview</a>
+		<td><a href="http://<?php print __CA_SITE_HOSTNAME__ . "/".__CA_URL_ROOT__."index.php/providenceTourML/providenceTourML/preview/id/".$tour["tour_id"]; ?>" class='form-button'><span class='form-button'><img src='<?php print "http://".__CA_SITE_HOSTNAME__ . "/".__CA_URL_ROOT__?>/themes/default/graphics/buttons/lens.png' border='0' alt='Preview' class='form-button-left' style='padding-right: 10px;'/>Preview</a>
 	</tr>
 
 <?php
